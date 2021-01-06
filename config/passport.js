8d8988bb20be270db56230a9296f9d774724d3e9
theshagod@gmail.com <theshagod@gmail.com>
2021-01-05 22:25:14 -0800

    adding bcryptjs and email auth

--- config/passport.js
+++ config/passport.js
@@ -4,28 +4,61 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 //the issue is the username
 module.exports = (passport) => {
+    /*
+    Tests:
+    POST .../api/login
+    {
+	"username": "jfon@gmail.com", //correct login info
+	"password": "123456"
+    } 
+    => passes
+    POST .../api/login
+    {
+	"username": "jfon", //correct login info
+	"password": "123456"
+    } 
+    =>passes
+
+    POST .../api/login
+    {
+	"username": "jfo", //wrong login info
+	"password": "123456"
+    } 
+    =>fails
+    POST .../api/login
+    {
+	"username": "jfon@gmail.com", //existing username but wrong pass info
+	"password": "1234567"
+    } 
+    => fails
+    POST .../api/login
+    {
+	"username": "jfon", //existing username but wrong pass info
+	"password": "1234567"
+    } 
+    => fails
+    */
     passport.use(new LocalStrategy(
     (username, password, done)=>{
-        User.findOne({where: {email:username}})
-            .then(user => {
-                //user not found
-                console.log('appear')
-                if(!user) {
-                    console.log('user not found')
-                    //check if matches with email
-                    return User.findOne({where: {email: email}})
-                        .then(user => {
-                            console.log('in email')
-                            if(!user) return done(null, false)
-                            //password doesn't match
-                            if (user.password != password) return done(null, false)
-                            return done(null, user)
-                        }).catch(err=>console.log('FFFF UUU'))
-                //password doesn't match
-                } else if (user.password != password) return done(null, false)
-                return done(null, user)
-            })
-            .catch(console.log)
+        const match = (field, cb) => {
+            let search = JSON.parse(`{"${field}":"${username}"}`)
+            User.findOne({where: search})
+                .then(user => {
+                    //user not found
+                    if(!user) {
+                        return cb()
+                    //password doesn't match
+                    } else if (bcrypt.compareSync(password, user.password)) return done(null, user)
+                    return done(null, false)
+                    
+                }).catch(console.log)
+        }
+        //finds matching username and if no matching, it will find a matching email
+        //if a matching username or email occurs, it will check the password to see
+        //if it is matching. done will be returned based on the success of operation
+        match("username",()=> {
+            match("email", () => done(null, false))
+        })
     }));
     /*
     Once user is authenticated (logs in with create credentials), which can be accomplished by
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 router.post('/signup', (req, res) => {
     if(!req.body.username || !req.body.password || !req.body.email){
         return res.send('must have username, password and email.')
     } else {
+        let hashedPass = bcrypt.hashSync(req.body.password , bcrypt.genSaltSync(10))
         User.create({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPass
         }).then(() => res.redirect('/login'))
         .catch(err => {
             console.log("User was not created. Something when wrong.")
