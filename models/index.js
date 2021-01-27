ed2b6d3abdd2fef87a5e92e0d6cf92fb9334e6d0
Jonathan <theshagod@gmail.com>
2021-01-26 20:24:55 -0800

    setup jawsdb

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize');
 const env =  process.env.NODE_ENV || "development"
-const config = require('../config/config.json')[env]
+const config = process.env.JAWSDB_URL || require('../config/config.json')[env]
 const sequelize = new Sequelize(config)
 
 
